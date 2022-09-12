02d59731f7052bf6848e144567d1397dcdd379bb
odulkeyt <odulkeyt@ibs.ru>
2022-09-12 16:44:54 +0600

    move .ignore file

---
+++ rest-assured-tests/src/test/java/pettype/TestRestClass.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.is;
+public class TestRestClass {
+    @Test
+    public void postmanFirstGetTest(){
+    RestAssured.
+            when().get("http://localhost:9966/petclinic/api/pettypes/1").
+            then().assertThat().statusCode(200).
+            and().body("name", is("cat"));
+    }
+}
